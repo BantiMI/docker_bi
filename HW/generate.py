4d18cf614635dd9d70912902fd85fbc7d4f73b1b
Иванов Максим <ivanovmaksim10012001@gmail.com>
2026-06-13 23:47:12 +0300

    Изменено наименование колонок

--- HW/generate.py
+++ HW/generate.py
@@ -6,15 +6,15 @@ import sys
 NUM_ROWS = 50
 
 
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["price", "discount", "quantity", "name"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "price": random.randint(0, 100),
+        "discount": round(random.uniform(1.5, 9.9), 2),
+        "quantity": random.randint(0, 100),
+        "name": random.choice(["iPhone 17", "iPhone 17 Pro Max", "iPhone 17 Pro"]),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
