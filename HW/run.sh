4917b6455fa4c481087d451af71c12dc9ead04fe
Иванов Максим <ivanovmaksim10012001@gmail.com>
2026-06-14 01:06:54 +0300

    Начало задание 2

--- HW/run.sh
+++ HW/run.sh
@@ -17,6 +17,17 @@ case "$1" in
         python3 generate.py local_data
         echo "Файл создан в local_data/data.csv"
         ;;
+
+    build_reporter)
+        echo "Сборка образа аналитика"
+        docker build -t reporter -f Dockerfile.reporter .
+        ;;
+    run_reporter)
+        echo "Запуск контейнера аналитика"
+        mkdir -p data
+        docker run --rm -v /mnt/d/docker_bi/HW/data:/data reporter
+        echo "Отчёт создан в data/report.html"
+        ;;
     *)
         echo "Использование: $0 {build_generator|run_generator|create_local_data}"
         exit 1
